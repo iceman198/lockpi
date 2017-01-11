8bcda8fb3fd564b0998dd96ffb9484dcde7519db
iceman198 <iceman198@gmail.com>
2017-01-11 14:24:19 -0500

    adding in button combo

--- index.js
+++ index.js
@@ -6,21 +6,55 @@ var logger = require('./logger.js');
 var accessKey = '098f6bcd4621d373cade4e832627b4f6'; // Access key given to you by the web app (http://locks.duttonbiz.com/)
 var lockId = '1'; // Lock id to identify the lock for your setup
 var codeArray = [];
+var sleepTime = 30000;
+var lockOpenTime = 5000;
 
 var relayPin = 16;
 rgpio.setup(relayPin, rgpio.DIR_OUT);
 
+var buttonIntTime = 200;
 var button1pin = 7;
+var button1value = true;
 rgpio.setup(button1pin, rgpio.DIR_IN);
+var button2pin = 11;
+var button2value = true;
+rgpio.setup(button2pin, rgpio.DIR_IN);
+var button3pin = 13;
+var button3value = true;
+rgpio.setup(button3pin, rgpio.DIR_IN);
+var button4pin = 15;
+var button4value = true;
+rgpio.setup(button4pin, rgpio.DIR_IN);
+
+var buttonCombo = '';
 
 setInterval(function() {
     rgpio.read(button1pin, function(err, value) {
-            console.log('The value of button1 is ' + value);
+            if (value !== button1value) {
+                button1value = value;
+                buttonChangeCall(1, value);
+            }
+            //logger.log('debug', 'index.js', 'The value of button1 is ' + value);
         });
-}, 500);
-
-var sleepTime = 30000;
-var lockOpenTime = 5000;
+    rgpio.read(button2pin, function(err, value) {
+            if (value !== button2value) {
+                button2value = value;
+                buttonChangeCall(2, value);
+            }
+        });
+    rgpio.read(button3pin, function(err, value) {
+            if (value !== button3value) {
+                button3value = value;
+                buttonChangeCall(3, value);
+            }
+        });
+    rgpio.read(button4pin, function(err, value) {
+            if (value !== button4value) {
+                button4value = value;
+                buttonChangeCall(4, value);
+            }
+        });
+}, buttonIntTime);
 
 getCodeList();
 setInterval(function() {
@@ -29,8 +63,25 @@ setInterval(function() {
 }, sleepTime);
 
 rc522(function(rfidNum) { // This is called everytime the reader sees a tag
-    if (codeArray.indexOf(rfidNum) > -1) {
-        logger.log('debug', 'index.js', 'RECOGNIZED rfid of ' + rfidNum + ' so Im letting them in');
+    checkCode(rfidNum);
+});
+
+function buttonChangeCall(button, value) {
+    if (value == false) {
+        buttonCombo = buttonCombo + '' + button;
+        logger.log('debug', 'index.js', 'Button combo set to ' + buttonCombo);
+    }
+    if (buttonCombo.length > 3) {
+        logger.log('debug', 'index.js', 'Button combo has reached 4 characters...resetting');
+        checkCode(buttonCombo);
+        buttonCombo = '';
+    }
+
+}
+
+function checkCode(code) {
+    if (codeArray.indexOf(code) > -1) {
+        logger.log('debug', 'index.js', 'RECOGNIZED code of ' + code + ' so Im letting them in');
         // some code to unlock the door
         setPin(relayPin, 0); // set the pin to low to trigger the relat
         setTimeout(function() {
@@ -39,12 +90,11 @@ rc522(function(rfidNum) { // This is called everytime the reader sees a tag
         }, lockOpenTime); // lock the door again after the set amount of time
         sendUnlockStatus('ALLOWED');
     } else {
-        logger.log('debug', 'index.js', 'UNKNOWN rfid of ' + rfidNum + ' // blocking access');
+        logger.log('debug', 'index.js', 'UNKNOWN code of ' + code + ' // blocking access');
         // some code to signal access denied
         sendUnlockStatus('BLOCKED');
     }
-});
-
+}
 
 function setPin(pin, stat) {
     var value = false;
