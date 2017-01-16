f2fa334301307737996c01d9d7a93dd01736aa1a
Sean Dutton <Sean Dutton>
2017-01-15 19:08:50 -0500

    b

--- index.js
+++ index.js
@@ -98,8 +98,8 @@ setTimeout(function() {
 
 function buzz(time) {
     for (i = 0; i < 1000; i++) {
-        var n = i*4;
-        var r = n+2;
+        var n = i*6;
+        var r = n+3;
         //logger.log('debug', 'index.js', 'N is ' + n + ' R is ' + r);
         setTimeout(function() {
             setPin(buzzerPin, 1);
