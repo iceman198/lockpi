15c187a59625ac8be19d00f596642b404baea7f4
iceman198 <iceman198@gmail.com>
2017-02-09 11:03:53 -0500

    b

--- unlock_success.py
+++ unlock_success.py
@@ -22,11 +22,11 @@ notes = {
 }
 
 melody_unlock_success = [
-	notes['D4'], notes['D4'], 0, notes['D4'], notes['C4'], notes['D4'], 0, notes['G4']
+	notes['D4'], notes['D4'], 0, notes['D4'], 0, notes['C4'], notes['D4'], 0, notes['G4']
 ]
 
 tempo_unlock_success = [
-	8,8,8,8,8,8,8,8
+	8,8,10,8,10,8,8,8,8
 ]
 
 def buzz(frequency, length):	 #create the function "buzz" and feed it the pitch and duration)
