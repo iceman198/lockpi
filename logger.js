804670d7e76865df599d06a2024391f1df4abac5
Sean Dutton <iceman198@gmail.com>
2019-01-04 14:38:32 -0500

    readable datetime

--- logger.js
+++ logger.js
@@ -17,7 +17,7 @@ function writeToLogFile(text) {
 	if (fileSizeInMegabytes > 5) {
 		var newFileName = logFile + '.old';
 		fs.rename(logFile, newFileName);
-		fs.writeFile(logFile, new Date() + ' - File created', function(err) {
+		fs.writeFile(logFile, getDateTime() + ' - File created', function(err) {
 			if(err) {
 				return console.log(err);
 			}
@@ -57,13 +57,36 @@ exports.log = function (level, desc, text) {
 	}
 	
 	//console.log('logLevelNum: ' + logLevelNum + ' // levelNum passed: ' + levelNum);
-	
-	var stringFile = '"' + new Date() + '","' + desc + '","' + text + '"';
-	var stringConsole = + new Date() + ' || ' + desc + ' || ' + text;
+	var mydate = getDateTime();
+	var stringFile = '"' + mydate + '","' + desc + '","' + text + '"';
+	var stringConsole = + mydate + ' || ' + desc + ' || ' + text;
 	if (logLevelNum <= levelNum) {
 		console.log(stringConsole);
 		writeToLogFile(stringFile);
 	} else {
 		console.log('NOT IN LOG ** ' + stringConsole);
 	}
+}
+
+function getDateTime() {
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+	var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+	return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
 }
